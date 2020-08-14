c8464c4921e599f78eff8a49ef8cbbdcad7b42b1
shivam19981998 <49227599+shivam19981998@users.noreply.github.com>
2020-08-14 07:17:22 +0530

    Message Changed

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace test
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Shivam!");
                 });
             });
         }
